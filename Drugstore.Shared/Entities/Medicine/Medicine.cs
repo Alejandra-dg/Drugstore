71c7eae4fd9515bc609f93276e6146af691df57f
SantiagoAlvarez17 <alvarezserna.17@gmail.com>
2023-04-27 08:10:41 -0500

    Medicine

--- Drugstore.Shared/Entities/Medicine/Medicine.cs
+++ Drugstore.Shared/Entities/Medicine/Medicine.cs
@@ -5,14 +5,52 @@ namespace Drugstore.Shared.Entities.Medicamento
 {
     public class Medicine
     {
-       
+        public int medicine_id { get; set; }
 
-            public int Id { get; set; }
 
-            [Display(Name = "Medicine")]  //{0}
-            [MaxLength(100, ErrorMessage = "Cuidado el campo {0} no permite más de {1} caracteres ")]  //{1}
-            [Required(ErrorMessage = "El campo {0} es obligatorio")]
-            public string? Name { get; set; } = null;
-        
+                [Display(Name = "Nombre")]
+                [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+                [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string medicine_name { get; set; } = null!;
+
+
+                [DataType(DataType.MultilineText)]
+                [Display(Name = "Descripción")]
+                [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        public string Description { get; set; } = null!;
+
+
+                [Column(TypeName = "decimal(18,2)")]
+                [DisplayFormat(DataFormatString = "{0:C2}")]
+                [Display(Name = "Precio")]
+                [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public decimal Price { get; set; }
+
+
+                [DisplayFormat(DataFormatString = "{0:N2}")]
+                [Display(Name = "Inventario")]
+                [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public float Stock { get; set; }
+
+
+       public ICollection<Category>? MedicineCategories { get; set; }
+
+
+                [Display(Name = "Categorías")]
+        public int MedicineCategoriesNumber => MedicineCategories == null ? 0 : MedicineCategories.Count;
+
+
+        public ICollection<MedicineImage>? MedicineImages { get; set; }
+
+
+                [Display(Name = "Imágenes")]
+        public int MedicineImagesNumber => MedicineImages == null ? 0 : MedicineImages.Count;
+
+
+             [Display(Name = "Imagen")]
+        public string MainImage => MedicineImages == null ? string.Empty : MedicineImages.FirstOrDefault()!.url_image;
+
+
+
     }
 }
