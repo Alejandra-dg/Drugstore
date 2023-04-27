1b1a600e7e421571333f76133c5a03596279f494
Maria Alejandra Davila Gonzalez <alejandragonza.07@gmail.com>
2023-04-27 18:25:52 -0500

    Configuración del DataContext

--- Drugstore.API/Date/DataContext.cs
+++ Drugstore.API/Date/DataContext.cs
@@ -1,52 +1,65 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using System.Data;
-using Microsoft.Extensions.Hosting;
-using Drugstore.API.Controllers;
-using Drugstore.Shared.Entities.Usuario;
-using Drugstore.Shared.Entities.Venta;
+using Drugstore.Shared.Entities;
 using Drugstore.Shared.Entities.Medicamento;
+using Drugstore.Shared.Entities.Medicine;
+//using Drugstore.Shared.Entities.Usuario;
+//using Drugstore.Shared.Entities.Venta;
+
 
 namespace Drugstore.API.Date
 {
     public class DataContext : DbContext
     {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
         //Medicamento
         public DbSet<Category> Categories { get; set; }
         public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public DbSet<MedicineCategory> MedicineCategories { get; set; }
-        public DbSet<MedicineImage>MedicineImages { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductImage> ProductImages { get; set; }
+        
 
 
 
         //Venta
-        public DbSet<MedicalOrder> MedicalOrders { get; set; }
-        public DbSet<Sale> Sales { get; set; }
+        //public DbSet<MedicalOrder> MedicalOrders { get; set; }
+        //public DbSet<Sale> Sales { get; set; }
 
 
 
 
         //Usuario
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Medicine>().HasIndex("CategoryId","Name").IsUnique();
             modelBuilder.Entity<MedicineCategory>().HasIndex("MedicineId", "Name").IsUnique();
-            //ORENES
+        
+            //ORDENES
 
-            modelBuilder.Entity<MedicalOrder>().HasIndex(x => x.order_id).IsUnique();
-            //MedicineImagne, cómo sería? 
-            modelBuilder.Entity<Sale>().HasIndex(x => x.sale_id).IsUnique();
-            modelBuilder.Entity<User>().HasIndex(x => x.user_name).IsUnique();
-            modelBuilder.Entity<Role>().HasIndex(d => d.role_name).IsUnique();
+        //modelBuilder.Entity<MedicalOrder>().HasIndex(x => x.order_id).IsUnique();
+        //MedicineImagne, cómo sería? 
+        //modelBuilder.Entity<Sale>().HasIndex(x => x.sale_id).IsUnique();
+        //modelBuilder.Entity<User>().HasIndex(x => x.user_name).IsUnique();
+        //modelBuilder.Entity<Role>().HasIndex(d => d.role_name).IsUnique();
+
+        
 
         }
-
     }
 }
