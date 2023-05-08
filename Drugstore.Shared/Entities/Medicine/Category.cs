0032097bce8ff6cc8809e154fb01888bdb7e13ab
Maria Alejandra Davila Gonzalez <alejandragonza.07@gmail.com>
2023-05-07 19:16:22 -0500

    Modificaciones Entidad Category

--- Drugstore.Shared/Entities/Medicine/Category.cs
+++ Drugstore.Shared/Entities/Medicine/Category.cs
@@ -4,25 +4,15 @@ namespace Drugstore.Shared.Entities.Medicamento
 {
     public class Category
     {
-        public object? MedicinesCategories;
-
         public int Id { get; set; }
 
-
-                [Display(Name = "Categoría")]
-                [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-                [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        public string? Name { get; set; } = null!;
-
-
-        //Realiza una colección de datos conjugados de categoría y medicamento
-        public ICollection<Medicine>? Medicines { get; set; }
+        [Display(Name = "Categoría")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
 
 
-        [Display(Name = "Medicamentos")]
-        //Para realizar el conteo de medicamentos por cada categoría
-        public int MedicinesNumber => Medicines == null ? 0 : Medicines.Count;
+        public ICollection<MedicineCategory>? ProductCategories { get; set; }
 
-       
     }
 }
