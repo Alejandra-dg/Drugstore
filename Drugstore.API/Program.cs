378f62697bb0a43be7090e5e518fe2688882e2c8
SantiagoAlvarez17 <alvarezserna.17@gmail.com>
2023-05-18 17:27:50 -0500

    Presentaciòn

--- Drugstore.API/Program.cs
+++ Drugstore.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "StoresG8G8 API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Drugstore API", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = @"JWT Authorization header using the Bearer scheme. <br /> <br />
